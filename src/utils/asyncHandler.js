d05f60a4a16b378d984d6613d608db6d70d6d9c1
syedsohaillahmed <syedsohaillahmed19@gmail.com>
2024-05-17 00:05:21 +0530

    added first router and created end point

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,10 @@
 const asyncHandler = (fn) => {
-  (req, res, next) => {
+ return (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
   };
 };
+
+export {asyncHandler}
 
 // try catch method for error handling
 
